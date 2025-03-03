d515843d808abf48062b7f0569db710cf84cbcfe
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-03-03 21:21:03 +0600

    for loop added

--- first_app/views.py
+++ first_app/views.py
@@ -3,4 +3,20 @@ from django.shortcuts import render
 # Create your views here.
 
 def contact(request):
-    return render(request, './first_app/index.html', context={'author': 'Phitron', 'age' : 22, 'marks' : 88})
+    return render(request, './first_app/index.html', {'courses': [
+        {
+            'id': 1,
+            'course': 'C',
+            'teacher': 'Abdullah'
+        },
+        {
+            'id': 2,
+            'course': 'C++',
+            'teacher': 'Al'
+        },
+        {
+            'id': 3,
+            'course': 'Python',
+            'teacher': 'Towsif'
+        }
+    ]})
